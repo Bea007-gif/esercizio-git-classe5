c9a18754faa4ab1f9e6d859eaec498235aca344f
CALVINO\I5DT7 <beatrice.mamone.2007@calvino.edu.it>
2025-11-27 16:23:46 +0100

    Aggiunta funzione somma

--- programma.c
+++ programma.c
@@ -1,6 +1,18 @@
+#include <stdio.h>
+int somma(int a, int b) {
+    return a + b;
+}
+
 main(){
 	printf("Hello world");
 	printf("Beatrice Mamone");
+	
+	 int x = 5;
+    int y = 7;
+
+    int risultato = somma(x, y);
+    printf("La somma è: %d\n", risultato);
+    
 	return 0;
 	
 }
